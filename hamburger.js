14abd7133914ca5d3405986691bff1f541861b26
Samuel Slivovsky <scool366@gmail.com>
2021-10-29 14:43:50 +0200

    formatting

--- hamburger.js
+++ hamburger.js
@@ -1,6 +1,5 @@
-
 var x = document.getElementById("myMenu");
 function myFunction(button) {
-    x.classList.toggle("responsive");
-    button.classList.toggle("active");
+  x.classList.toggle("responsive");
+  button.classList.toggle("active");
 }
